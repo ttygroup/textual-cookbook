d67159aada744deb889f9ea0cbceb687f081ef54
Edward Jazzhands <ed.jazzhands@gmail.com>
2025-08-01 20:31:52 -0300

    cleanup because of new reports branch (#3)

---
+++ examples/tips_and_tricks/reveal_redacted.py
@@ -0,0 +1,78 @@
+"""This script demonstrates how you could set redacted text and then reveal
+the text when hovered.
+The RedactedText widget has a reveal_range reactive attribute which is updated
+when some redacted text is hovered. This will prompt a "smart refresh" to update
+which ranges have the 'redacted' styling.
+The important part is the metadata (meta) applied to portions of the text.
+The mouse event handler checks the event.style.meta and updates the reveal_range
+reactive attribute accordingly.
+
+Example by Tom J Gooding, 2025"""
+
+from __future__ import annotations  # <-- Added by Edward Jazzhands for 3.9 compatibility
+from rich.text import Text
+from textual import events
+from textual.app import App, ComposeResult, RenderResult
+from textual.reactive import reactive
+from textual.widget import Widget
+
+
+class RedactedText(Widget):
+    COMPONENT_CLASSES = {"redacted"}
+
+    DEFAULT_CSS = """
+    RedactedText {
+        width: auto;
+        height: auto;
+
+        .redacted {
+            color: $foreground;
+            background: $foreground;
+        }
+    }
+    """
+
+    reveal_range: reactive[tuple[int, int] | None] = reactive(None)
+
+    def render(self) -> RenderResult:
+        redacted_style = self.get_component_rich_style("redacted")
+
+        redacted_text = Text("Hello world foo bar")
+        redact_ranges = [(6, 11), (16, 19)]
+
+        for redact_range in redact_ranges:
+            start, end = redact_range
+            # Add meta info - see the mouse handler below
+            redacted_text.apply_meta({"redacted": redact_range}, start, end)
+
+            if self.reveal_range != redact_range:
+                # Add a redacted style to portions of the text
+                redacted_text.stylize(redacted_style, start, end)
+
+        return redacted_text
+
+    def on_mouse_move(self, event: events.MouseMove) -> None:
+        meta = event.style.meta
+        if "redacted" in meta:
+            self.reveal_range = meta["redacted"]
+        else:
+            self.reveal_range = None
+
+    def on_leave(self) -> None:
+        self.reveal_range = None
+
+
+class RedactedApp(App):
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        yield RedactedText()
+
+
+if __name__ == "__main__":
+    app = RedactedApp()
+    app.run()
